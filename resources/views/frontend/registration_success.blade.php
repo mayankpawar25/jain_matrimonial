3ee10c82e63e5998ada2155d7e36f3b4cbf7c60d
Mayank <mayank.pawar@webcraftit.com>
2024-09-27 17:35:32 +0530

    success page UI design

--- resources/views/frontend/registration_success.blade.php
+++ resources/views/frontend/registration_success.blade.php
@@ -4,14 +4,22 @@
 <link rel="stylesheet" href="{{ static_asset('assets/css/registration.css') }}">
 
 <div class="py-5 mt-5 py-lg-5 mt-2">
-    <div class="containter">
+    <div class="container">
         <div class="row">
-            <div class="form-wizard p-4 bg-white shadow rounded">
-                <h1>Registration Successful!</h1>
-                <p>Thank you for registering. Your Registration ID is: <strong>{{ $registration->id }}</strong></p>
+           <div class="col-md-12">
+           <div class="form-wizard p-4 bg-white shadow rounded text-center ">
+                <img src="{{ static_asset('assets/img/success-img.png') }}" style="max-width: 150px;" alt="">
+                <h1 class="mt-5" style="font-weight: 600; color: #ee2098;"><strong>{{ $registration->name }}, </strong> आपका रजिस्ट्रेशन सफलता पूर्वक हो गया है!</h1>
+                <br>
+                <h4>आपका रजिस्ट्रेशन नंबर है : <strong>#DJSGF-100{{ $registration->id }}</strong><br> <br>
+                कृपया यह नंबर अपने पास सुरक्षित रखे.</h3>
+
+                <a href="/" class="btn btn-next width mt-3" style="background-color: rgb(240, 53, 162); border: none; color: white;" >Back To Home</a>
+                <!-- <p>Thank you for registering. Your Registration ID is: <strong>{{ $registration->id }}</strong></p>
                 <p>Name: {{ $registration->name }}</p>
-                <p>Email: {{ $registration->email }}</p>
+                <p>Email: {{ $registration->email }}</p> -->
             </div>
+           </div>
         </div>
     </div>
 </div>
