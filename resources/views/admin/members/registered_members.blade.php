13160c6304ac2cb5b7f108c56639168f99b81d7d
Aakash <aakash.sengar@webcraftit.com>
2024-10-04 15:57:05 +0530

    photo modal border none admin panel registered members

--- resources/views/admin/members/registered_members.blade.php
+++ resources/views/admin/members/registered_members.blade.php
@@ -153,16 +153,13 @@
     </div>
 </div>
    <!-- Image Modal -->
-   <div class="modal fade" id="imageModal" tabindex="-1" role="dialog" aria-labelledby="imageModalLabel" aria-hidden="true">
+   <div class="modal fade " id="imageModal" tabindex="-1" role="dialog" aria-labelledby="imageModalLabel" aria-hidden="true">
                             <div class="modal-dialog" role="document">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <!-- <h5 class="modal-title" id="imageModalLabel">{{ translate('Image') }}</h5> -->
-                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                            <span aria-hidden="true">&times;</span>
-                                        </button>
-                                    </div>
+                                <div class="modal-content" style="background : transparent; border: none">
                                     <div class="modal-body">
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                        </button>
                                         <img id="modalImage" src="" class="img-fluid w-100" alt="{{ translate('image') }}">
                                     </div>
                                 </div>
